8d8fd4954b72675f270094642dd116017c00e36f
PavanKumar110 <lavanyapavan346@gmail.com>
2025-04-10 22:12:27 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+import Demo from "./Demo"
+
+function App() {
+  return(
+    <div>
+      <h1>
+        Demo React App
+        <Demo/>
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Demo.jsx
@@ -0,0 +1,13 @@
+//import React from "react";
+
+function Demo() 
+{
+    return(
+        <>
+        <p>KL University</p>
+        <p>CSE Department</p>
+        <p style={{color:"red"}}>K PAVAN KUMAR</p>
+        </>
+    )
+}
+export default Demo
